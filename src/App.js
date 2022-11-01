4d5b1d3f5bf7a1420439d98b6fc3fea6724f14ea
AngelGonzs <angel.gonzalez49@upr.edu>
2022-11-01 14:22:05 -0400

    Adding all react files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import UserRegistration from './Components/UserRegistration.tsx'
+
+function App() {
+  return (
+
+    <div className="App">
+      <UserRegistration />
+    </div>
+  );
+}
+
+export default App;
