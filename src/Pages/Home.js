df163293d2458c29ad18ad8c4d1c2ead0b9c057f
CarolinaZRM <carolina.rodriguez11@upr.edu>
2022-11-14 02:34:23 -0400

    Fixed background issue

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,9 +3,9 @@ import logo from '../Images/petHealthLogo.png'
 
 function Home() {
     return (
-      <div>
-        <h1> Welcome to!</h1>
-        <img src= "https://i.postimg.cc/qvYVqcRL/App-Logo-Home-Page.png"  />
+      <div align="center">
+        <h1> Welcome to</h1>
+        <img src= "https://i.postimg.cc/brKxWVGs/Fixed-Logo-removebg-preview.png"  />
       </div>
     );
 }
