a806dbcbb0145bd86118b7d19c5ca0516ff60ae2
Alex202-0 <alexander.ibarrondo@upr.edu>
2022-11-30 16:27:48 -0400

    added the option in the profile to add a pfp

--- src/Pages/profile.js
+++ src/Pages/profile.js
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import './profile.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Nav, NavLink, NavMenu } from "../Components/NavbarElements";
   
 const Profile = ({userData}) => {
+  const [selectedImage, setSelectedImage] = useState(null);
   return (
     <div>
-      {userData.userName !== "" ? (
-        <div>
-          <h1>Your profile</h1>
+      {userData.userName == "" ? (
+        
+        <div className="userInfo">
+          <h1>Profile Picture:</h1>
+        {selectedImage && (
+          <div>
+          <img alt="not fount" width={"250px"} src={URL.createObjectURL(selectedImage)} />
+          <br />
+          <button onClick={()=>setSelectedImage(null)}>Remove</button>
+          </div>
+        )}
+        <br />
+
+        <br /> 
+        <input
+          type="file"
+          name="myImage"
+          onChange={(event) => {
+            console.log(event.target.files[0]);
+            setSelectedImage(event.target.files[0]);
+          }}
+        />
+          <h1>Profile Information:</h1>
           <ls>
-            <li>Your name is{userData.userName}</li>
-            <li>Your email is {userData.email}</li>
-            <li>Your password is {userData.password}</li>
+            <li>Username: {userData.userName}</li>
+            <li>Email: {userData.userName}</li>
           </ls>
         </ div>
         
         ) : (
           <div>
-            <h1>Register Here Now!</h1>
+            <h1>Log into your account!</h1>
             <p>
-              <Link to ="/logInPage" >Click Here to Log In!</Link>
+              <Link to ="/logInPage" >Click here to log in!</Link>
             </p>
           </div>
         )}
